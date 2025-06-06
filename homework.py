8bb432ee8014fe204fc81f90a917843513174d71
Elena Tinkalyuk <lenalenalena3@mail.ru>
2025-06-06 10:51:57 +0300

    Результат ДЗ для лекции 6

--- homework.py
+++ homework.py
@@ -9,6 +9,8 @@ def test_dark_theme_by_time():
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
     is_dark_theme = None
+    if not (6 < current_time.hour < 22):
+        is_dark_theme = True
     assert is_dark_theme is True
 
 
@@ -26,6 +28,15 @@ def test_dark_theme_by_time_and_user_choice():
     #  но учтите что темная тема может быть включена вручную
 
     is_dark_theme = None
+    if dark_theme_enabled_by_user:
+        is_dark_theme = True
+    elif dark_theme_enabled_by_user == False:
+        is_dark_theme = False
+    else:
+        if (6 < current_time.hour < 22):  # день
+            is_dark_theme = False
+        else:
+            is_dark_theme = True
     assert is_dark_theme is True
 
 
@@ -43,10 +54,17 @@ def test_find_suitable_user():
 
     # TODO найдите пользователя с именем "Olga"
     suitable_users = None
+    for user in users:
+        if user['name'] == 'Olga':
+            suitable_users = user
+            break
     assert suitable_users == {"name": "Olga", "age": 45}
 
     # TODO найдите всех пользователей младше 20 лет
-    suitable_users = None
+    suitable_users = []
+    for user in users:
+        if user['age'] < 20:
+            suitable_users.append(user)
     assert suitable_users == [
         {"name": "Stanislav", "age": 15},
         {"name": "Maria", "age": 18},
@@ -70,16 +88,24 @@ def test_readable_function():
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
 
+def print_name_func(function, *args):
+    func_name = function.__name__.replace("_", " ").title()
+    args_str = ', '.join(map(str, args))
+    func_arg = f"{func_name} [{args_str}]"
+    print(func_arg)
+    return func_arg
+
+
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = print_name_func(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = print_name_func(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = print_name_func(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
